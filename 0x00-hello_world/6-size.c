aca5a974802a20478087a20814700ea747152b40
clybowl <4941@holbertonschool.com>
2022-05-20 11:29:58 -0700

    task 6

---
+++ 0x00-hello_world/6-size.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+
+/**
+ *  * main - Entry point
+ *   *
+ *    * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	printf(" int * is %2d bytes \n", sizeof(int *));
+	return (0);
+}
