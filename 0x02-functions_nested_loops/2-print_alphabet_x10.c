69a8c67b158c0aa7bfd72c8eccd1aca674468495
clybowl <4941@holbertonschool.com>
2022-05-25 09:31:15 -0700

    task 2

--- 0x02-functions_nested_loops/2-print_alphabet_x10.c
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -1,10 +1,11 @@
-#include "holberton.h"
+#include "main.h"
 
 /**
- * main - check the code
+ * print_alphabet_x10 - prints 10 times the alphabet, in lowercase,
+ * on a new line
  *
  * Return: Always 0.
- * 
+ *
  */
 
 void print_alphabet_x10(void)
