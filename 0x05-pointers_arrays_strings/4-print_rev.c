0c397a505e29c40c15be2127dd33cc90c40bd66a
clybowl <4941@holbertonschool.com>
2022-06-03 19:18:35 -0700

    task 4

--- 0x05-pointers_arrays_strings/4-print_rev.c
+++ 0x05-pointers_arrays_strings/4-print_rev.c
@@ -1,1 +1,22 @@
 #include "main.h"
+
+/**
+* _rev -  prints string in reverse, followed by new line
+* @s: string
+* Return: returns 0.
+*/
+void print_rev(char *s)
+{
+	int f1;
+	int f2;
+
+	for (f1 = 0; s[f1] != '\0'; f1++)
+	{
+		continue;
+	}
+	for (f2 = f1 - 1; f2 >= 0; f2--)
+	{
+		_putchar(s[f2]);
+	}
+	_putchar('\n');
+}
