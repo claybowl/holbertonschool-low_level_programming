033812a52fbfb2d4deb878a1086366b594d823db
clybowl <4941@holbertonschool.com>
2022-06-03 19:56:34 -0700

    task 6

--- 0x05-pointers_arrays_strings/6-puts2.c
+++ 0x05-pointers_arrays_strings/6-puts2.c
@@ -1,1 +1,27 @@
 #include "main.h"
+
+/**
+* puts2 - prints every other character of a string
+* starting with first character
+* @str: string
+* Return: void
+*/
+
+void puts2(char *str)
+{
+int f1;
+int f2;
+
+for (f2 = 0; str[f2] != '\0'; f2++)
+{
+	f2 -= 1;
+	for (f1 = 0; f1 <= f2; f1++)
+	{
+		if (f1 % 2 == 0)
+		{
+			_putchar(str[f1]);
+		}
+_putchar('\n');
+	}
+}
+}
