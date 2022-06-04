fd54ea09dcb94d405106d04defefeefcc542090b
clybowl <4941@holbertonschool.com>
2022-06-03 17:28:44 -0700

    task 2

--- 0x05-pointers_arrays_strings/2-strlen.c
+++ 0x05-pointers_arrays_strings/2-strlen.c
@@ -1,1 +1,18 @@
 #include "main.h"
+
+/**
+* int_strlen -  swaps the value of two integers
+* @s: variable
+* Return: return length of string
+*/
+int _strlen(char *s)
+{
+	int length1;
+
+	for (length1 = 0; *s !='\0'; s++)
+	{
+		length1++;
+	}
+
+	return (length1);
+}
