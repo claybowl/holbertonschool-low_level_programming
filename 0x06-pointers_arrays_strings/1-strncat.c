fd946de01bef2422e69b60a2fa3c13b15819cae6
clybowl <4941@holbertonschool.com>
2022-06-06 06:46:34 -0700

    task 1

---
+++ 0x06-pointers_arrays_strings/1-strncat.c
@@ -0,0 +1,20 @@
+#include "holberton.h"
+
+/**
+* _strncat - concatenates two strings
+* @dest - destination string
+* @src - source string
+* Return: a pointer to the resulting string dest
+*/
+
+char *_strncat(char *dest, char *src, int n)
+{
+	int f1, f2;
+
+	f1 = 0;
+	f2 = 0;
+
+	while (dest[f1] != '\0')
+		f1++;
+
+	while (src[f2] != '\0' &&
