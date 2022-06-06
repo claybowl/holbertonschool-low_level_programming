dce3b3fb13c892407c356d4269f49ad1f88cb0b3
clybowl <4941@holbertonschool.com>
2022-06-06 10:23:12 -0700

    task 2

--- 0x06-pointers_arrays_strings/1-strncat.c
+++ 0x06-pointers_arrays_strings/1-strncat.c
@@ -1,9 +1,19 @@
-#include "holberton.h"
+{
+               size_t i;
+
+               for (i = 0; i < n && src[i] != '\0'; i++)
+                   dest[i] = src[i];
+               for ( ; i < n; i++)
+                   dest[i] = '\0';
+
+               return dest;
+           }#include "holberton.h"
 
 /**
 * _strncat - concatenates two strings
-* @dest - destination string
-* @src - source string
+* @dest: destination string
+* @src: source string
+* @n: number of bytes from src
 * Return: a pointer to the resulting string dest
 */
 
