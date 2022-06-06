fd22586a3c8aa146acac59813ffbef7639323e2a
clybowl <4941@holbertonschool.com>
2022-06-06 15:23:45 -0700

    task 7

--- 0x06-pointers_arrays_strings/7-leet.c
+++ 0x06-pointers_arrays_strings/7-leet.c
@@ -3,13 +3,27 @@
 /**
 * leet - capitalizes all words of a string
 *
-* @p: string variable
+* @s: string variable
 *
 * Return: encodes a long string into 1337
 */
-char *leet(char *p)
+char *leet(char *s)
 {
+	int i, j;
 
+	char *a = "aAeEoOtTlL";
+	char *b = "4433007711";
 
-	return (p);
+	for (i = 0; s[i] != '\0'; i++)
+	{
+		for (j = 0; j < 10; j++)
+		{
+			if (s[i] == a[j])
+			{
+				s[i] = b[j];
+			}
+		}
+	}
+
+	return (s);
 }
