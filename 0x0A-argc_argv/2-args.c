a5386ea70bc34852ca35326a95d15aac868d65c0
clybowl <4941@holbertonschool.com>
2022-06-15 08:26:28 -0700

    task 0

--- 0x0A-argc_argv/2-args.c
+++ 0x0A-argc_argv/2-args.c
@@ -1,1 +1,17 @@
 #include <stdio.h>
+
+/**
+* main - print arguments it recieves
+* @argc: number count
+* @argv: name
+* Return: 0
+*/
+
+int main(int argc, char *argv[])
+{
+	int i = 0;
+	for (; i < argc; i++)
+	printf("%s\n", argv[i]);
+	return (0);
+	
+}
