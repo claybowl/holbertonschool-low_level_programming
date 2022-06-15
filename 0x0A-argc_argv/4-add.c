672ec4cb42f75fcdbb225098802bccf50bf9feec
clybowl <4941@holbertonschool.com>
2022-06-15 09:00:13 -0700

    task 4

--- 0x0A-argc_argv/4-add.c
+++ 0x0A-argc_argv/4-add.c
@@ -1,1 +1,30 @@
 #include <stdio.h>
+#include <stdlib.h>
+
+/**
+* main - adds positive numbers
+* @argc: argument count
+* @argv: argument vector
+* Return: 0 on success
+*/
+
+int main(int argc, char *argv[])
+{
+	int num, digit, sum = 0;
+
+	for (num = 1; num < argc; num++)
+	{
+		for (digit = 0; argv[num][digit]; digit++)
+		{
+			if (argv[num][digit] < '0' || argv[num][digit] > '0')
+			{
+				printf("Error\n");
+				return (1);
+			}
+		}
+		sum += atoi(argv[num]);
+	}
+	printf("%d\n", sum);
+
+	return (0);
+}
