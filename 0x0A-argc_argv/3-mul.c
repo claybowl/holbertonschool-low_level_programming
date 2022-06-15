418bbd554e82dab35f24d207f8ffb7396947b49a
clybowl <4941@holbertonschool.com>
2022-06-15 08:48:30 -0700

    task 3

--- 0x0A-argc_argv/3-mul.c
+++ 0x0A-argc_argv/3-mul.c
@@ -1,1 +1,26 @@
 #include <stdio.h>
+#include <stdlib.h>
+
+/**
+* main - multiply two numbers
+* @argc: argument count
+* @argv: argument vector
+* Return: 0.
+*/
+
+int main(int argc, char *argv[])
+{
+	int mult_num = 0;
+
+	if (argc <= 2)
+	{
+		printf("Error\n");
+		return (1);
+	}
+	else
+	{
+	mult_num = atoi(argv[1]) * atoi(argv[2]);
+	printf("%d\n", mult_num);
+	return (0);
+	}
+}
