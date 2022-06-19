274298bc0e307e549c14c5089c26ce7c9ff5c510
clybowl <4941@holbertonschool.com>
2022-06-19 07:43:02 -0700

    task 2

---
+++ 0x0B-malloc_free/2-str_concat.c
@@ -0,0 +1,66 @@
+#include <stdlib.h>
+#include "main.h"
+
+int _strlen(char *s);
+
+/**
+ * str_concat - concatenates two strings and dynamically allocates its memory
+ * @s1: the leading string
+ * @s2: the string to follow
+ * Return: a pointer to a newly allocagtd space in memory that contains s1 & s2
+ * or NULL on failure of memory allocation
+ */
+
+char *str_concat(char *s1, char *s2)
+{
+	int i = 0;
+	char *j; /* this is the pointer to the two concatenated strings */
+
+	if (s1 == NULL)
+	{
+		s1 = "\0";
+	}
+	if (s2 == NULL)
+	{
+		s2 = "\0";
+	}
+/* dynamically allocate memory for the two concatenated strings */
+	j = malloc(_strlen(s1) + _strlen(s2) + 1);
+
+	/* if memory cannot be allocated return null for failure */
+	if (j == NULL)
+	{
+		return (NULL);
+	}
+	while (*s1 != '\0')
+	{
+		j[i] = *s1;
+		s1++;
+		i++;
+	}
+	while (*s2 != '\0')
+	{
+		j[i] = *s2;
+		s2++;
+		i++;
+	}
+	return (j);
+}
+
+/**
+ * _strlen  -  returns the length of a string
+ * @s: s is a string
+ * Return: return length of string for success
+*/
+
+int _strlen(char *s)
+{
+	int purp = 0;
+
+	while (*s != '\0')
+	{
+		purp++;
+		s++;
+	}
+	return (purp);
+}
