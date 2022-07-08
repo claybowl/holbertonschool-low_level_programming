ddc58e947e8751ab4130799b04046b8cdf625da0
clybowl <4941@holbertonschool.com>
2022-07-08 12:12:51 -0700

    task 0

--- 0x12-singly_linked_lists/0-print_list.c
+++ 0x12-singly_linked_lists/0-print_list.c
@@ -6,4 +6,23 @@
 size_t print_list(const list_t *h)
 {
 	size_t count = 0;
+	const list_t *current = head;
+	char *string;
+	unsigned int length;
+
+	while (current != NULL)
+	{
+		count++;
+		if (current->str == NULL)
+		{
+			printf("[0] (nil)\n");
+			count++;
+			current = current->next;
+		}
+		string = current->str;
+		length = current->len;
+		printf("[%u] %s\n", length, string);
+		current = current->next;
+	}
+	return (count);
 }
--- 0x12-singly_linked_lists/lists.h
+++ 0x12-singly_linked_lists/lists.h
@@ -1,13 +1,33 @@
-#ifndef
-#define
+#ifndef LISTS_H
+#define LISTS_H
 
 /* includes */
 
 #include <unistd.h>
 #include <stdio.h>
-#include <stdarg.h>
+#include <stdlib.h>
+
+/* structures */
+
+/**
+* struct list_s - singly linked list
+* @str: string - (malloc'ed string)
+* @len: length of the string
+* @next: points to the next node
+*
+* Description: singly linked list node structure
+*/
+
+typedef struct list_s
+{
+	char *str;
+	unsigned int len;
+	struct list_s *next;
+} list_t;
 
 /* prototypes */
+
+int _strlen(char *s);
 
 int _putchar(char c);
 
@@ -20,22 +40,5 @@ list_t *add_node(list_t **head, const char *str);
 list_t *add_node_end(list_t **head, const char *str);
 
 void free_list(list_t *head);
-
-/* data structure */
-
-/**
- * struct list_s - singly linked list
- * @str: string - (malloc'ed string)
- * @len: length of the string
- * @next: points to the next node
- *
- * Description: singly linked list node structure
- */
-typedef struct list_s
-{
-    char *str;
-    unsigned int len;
-    struct list_s *next;
-} list_t;
 
 #endif
