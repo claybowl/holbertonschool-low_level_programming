92aa7f2244dc509da6794494ff91734d67541a1a
clybowl <4941@holbertonschool.com>
2022-07-08 20:05:07 -0700

    task 1

---
+++ 0x12-singly_linked_lists/1-list_len.c
@@ -0,0 +1,20 @@
+#include "list.h"
+
+/*
+* list_len - returns number of elements in linked list list_t
+* @head: points to the linked list list_t
+* Return: the number of elements in linked list
+*/
+
+size_t list_len(const list_t *head);
+	{
+		size_t count = 0;
+		const list_t *current = head;
+
+		while (current != NULL)
+		{
+			count++;
+			current = current->next;
+		}
+		return (count);
+	}
