c20be2bb9d19715a3462f79a69a1adb214b0bb13
clybowl <4941@holbertonschool.com>
2022-07-13 12:21:20 -0700

    task 1

---
+++ 0x14-bit_manipulation/1-print_binary.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+#include <limits.h>
+#include "main.h"
+
+/**
+*  -
+* @
+* Return:
+*/
+
+void print_binary(unsigned long int n)
+{
+	int i;
+	int c = sizeof(int) * CHAR_BIT;
+	int mask = 1 << (c - 1);
+
+	for (i = 1; i <= c; ++i)
+	{
+		_putchar(((n & mask) == 0) ? '0' : '1');
+		n <<= 1;
+		if (i % CHAR_BIT == 0 && i < c)
+			_putchar(' ');
+	}
+}
