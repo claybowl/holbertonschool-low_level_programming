ad2c0f35b2b00d8874069cdbcce89da885940a02
clybowl <4941@holbertonschool.com>
2022-07-12 18:42:27 -0700

    task 0

---
+++ 0x13-more_singly_linked_lists/0-print_listint.c
@@ -0,0 +1,22 @@
+#include "lists.h"
+
+/**
+* print_listint - function that prints all of the of a list_s list
+* @head: pointer to listint_t list, a singly linked list.
+*
+*
+*
+*/
+
+size_t print_listint(const listint_t *head);
+{
+	unsigned int count = 0;
+	const listint_t = head;
+	
+	while (current != NULL)
+	{
+		count++;
+		printf("%d/n", current->next;
+	}
+	return (count);
+}
