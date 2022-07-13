0ddda1f30c9bfdd4f41e1687187fc9d74378ddcb
clybowl <4941@holbertonschool.com>
2022-07-13 10:56:00 -0700

    task 0

--- 0x14-bit_manipulation/0-binary_to_uint.c
+++ 0x14-bit_manipulation/0-binary_to_uint.c
@@ -15,11 +15,15 @@ unsigned int binary_to_uint(const char *b)
 	if (b == NULL)
 		return (0);
 
-	while (b[i] == '0' || b[i] == '1')
+	while (b[i] == 48 || b[i] == 49)
 	{
 		val <<= 1;
 		val += b[i] - '0';
 		i++;
+		if (b[i] >= '2')
+		{
+			return (0);
+		}
 	}
 	return (val);
 }
