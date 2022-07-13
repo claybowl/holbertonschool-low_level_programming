2106e74ce67d1ae0ab0126aa250799ff64ce6595
clybowl <4941@holbertonschool.com>
2022-07-13 13:14:34 -0700

    task 2

--- 0x14-bit_manipulation/2-get_bit.c
+++ 0x14-bit_manipulation/2-get_bit.c
@@ -8,10 +8,13 @@
 
 int get_bit(unsigned long int n, unsigned int index)
 {
+	int bit;
+
 	if (index > 63)
 	{
 		return (-1);
 	}
-	n = n >> index;
-	return (1 $ n);
+	bit = (n >> index) & 1;
+
+	return (bit);
 }
