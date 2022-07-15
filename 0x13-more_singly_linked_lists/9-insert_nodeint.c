0f6b8c7c62f79653e0bae2eb27279cb6117982a4
clybowl <4941@holbertonschool.com>
2022-07-14 21:48:08 -0700

    task 9

--- 0x13-more_singly_linked_lists/9-insert_nodeint.c
+++ 0x13-more_singly_linked_lists/9-insert_nodeint.c
@@ -1,36 +1,45 @@
 #include "lists.h"
 
 /**
- * insert_nodeint_at_index - inserts a new node at a given position
- * @head: pointer to the pointer of listint_t linked list
- * @idx: the index of the list where the new node should be added, stars at 0
- * @new: integer that holds value of new node
- * Return: the address of the new node or NULL if it fails
+ * insert_nodeint_at_index - inserts a new node in a linked list,
+ * at a given position
+ * @head: pointer to the first node in the list
+ * @idx: index where the new node is added
+ * @n: data to insert in the new node
+ *
+ * Return: pointer to the new node, or NULL
  */
-
-listint_t *insert_nodeint_at_index(listint_t **head, unsigned int idx, int new)
+listint_t *insert_nodeint_at_index(listint_t **head, unsigned int idx, int n)
 {
-	listint_t *current = *head;
-	listint_t *new_node;
-	unsigned int count = 0;
+	unsigned int i;
+	listint_t *new;
+	listint_t *temp = *head;
 
-	new_node = malloc(sizeof(listint_t));
-	if (new_node == NULL)
-	{
-		free(new_node);
+	new = malloc(sizeof(listint_t));
+	if (!new || !head)
 		return (NULL);
-	}
-	new_node->n = new;
-	while (current != NULL)
+
+	new->n = n;
+	new->next = NULL;
+
+	if (idx == 0)
 	{
-		if (count == idx - 1)
-		{
-			new_node->next = current->next;
-			current->next = new_node;
-			return (new_node);
-		}
-		count++;
-		current = current->next;
+		new->next = *head;
+		*head = new;
+		return (new);
 	}
+
+	for (i = 0; temp && i < idx; i++)
+	{
+		if (i == idx - 1)
+		{
+			new->next = temp->next;
+			temp->next = new;
+			return (new);
+		}
+		else
+			temp = temp->next;
+	}
+
 	return (NULL);
 }
