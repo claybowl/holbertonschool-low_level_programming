5ec5cc251d320d75c479f880c7fa6a974d51770c
clybowl <4941@holbertonschool.com>
2022-07-18 19:06:22 -0700

    task 2

--- 0x15-file_io/2-append_text_to_file.c
+++ 0x15-file_io/2-append_text_to_file.c
@@ -1,82 +1,56 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include "main.h"
+
 /**
-* err_argc - argc error
-* @argc: number of arguments
+* _strlen - returns the length of a string
+* @s: character
+*
+* Return: Always 0 (Success)
 */
-void err_argc(int argc)
+
+int _strlen(char *s)
 {
-	if (argc != 3)
+	int len = 0;
+
+	while (*s != '\0')
 	{
-		dprintf(STDERR_FILENO, "Usage: cp file_from file_to\n");
-		exit(97);
+		len++;
+		s++;
 	}
+
+	return (len);
 }
 
 /**
-* err_close - close error
-* @a: file descriptor
+* append_text_to_file - appends text at the end of a file
+* @filename: filename
+* @text_content: NULL terminated string to add at the end of the file
+* Return: 1 on success and -1 on failure
 */
 
-void err_close(int a)
+int append_text_to_file(const char *filename, char *text_content)
 {
-	dprintf(STDERR_FILENO, "Error: Can't close fd %d\n", a);
-	exit(100);
-}
+	int fd, len;
+	int w;
 
-/**
-* err_write - write error
-* @s: string
-*/
-
-void err_write(char *s)
-{
-	dprintf(STDERR_FILENO, "Error: Can't write to %s\n", s);
-	exit(99);
-}
-
-/**
-* main- main function
-* @argc: number of arguments
-* @argv: arguments
-* Return: Always 0.
-*/
-int main(int argc, char **argv)
-{
-	int fd, fdcp, w, r;
-	char buffer[1024];
-
-	err_argc(argc);
-	fdcp = open(argv[2], O_WRONLY | O_CREAT | O_TRUNC, 0664);
-	if (fdcp == -1)
-		err_write(argv[2]);
-	fd = open(argv[1], O_RDONLY);
+	if (filename == NULL)
+		return (-1);
+	fd = open(filename, O_WRONLY | O_APPEND);
 	if (fd == -1)
+		return (-1);
+	if (text_content == NULL)
+		return (1);
+
+	len = _strlen(text_content);
+	w = write(fd, text_content, len);
+	if (w == -1)
 	{
-		dprintf(STDERR_FILENO, "Error: Can't read from file %s\n", argv[1]);
-		close(fdcp);
-		exit(98);
-	}
-	while ((r = read(fd, buffer, 1024)) != 0)
-	{
-		if (r == -1)
-		{
-			dprintf(STDERR_FILENO, "Error: Can't read from file %s\n", argv[1]);
-			close(fdcp);
-			exit(98);
-		}
-		w = write(fdcp, buffer, r);
-		if (w == -1)
-			err_write(argv[2]);
-	}
-	if (close(fd) == -1)
-		err_close(fd);
-	else
 		close(fd);
-	if (close(fdcp) == -1)
-		err_close(fdcp);
-	else
-		close(fdcp);
-	return (0);
+		return (-1);
+	}
+
+	close(fd);
+	return (1);
+
 }
