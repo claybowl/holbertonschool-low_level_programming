5a520ef84abcb870f062e07d1fef71775add4e57
clybowl <4941@holbertonschool.com>
2022-08-01 13:27:20 -0700

    README and lists.h setup

--- 0x18-doubly_linked_lists/lists.h
+++ 0x18-doubly_linked_lists/lists.h
@@ -2,12 +2,28 @@
 #define MAIN_H
 
 /* includes */
->
+
 #include <string.h.h>
 #include <stdio.h>
 #include <stdlib.h>
 
 /* structures */
+
+/**
+* struct dlistint_s - doubly linked list
+* @n: integer
+* @prev: points to the previous node
+* @next: points to the next node
+*
+* Description: doubly linked list node structure
+*
+*/
+typedef struct dlistint_s
+{
+	int n;
+	struct dlistint_s *prev;
+	struct dlistint_s *next;
+} dlistint_t;
 
 /* prototypes */
 
