7a2841269516714d4fc943437c047bd250e3b362
claybowl <claybowlproductions@gmail.com>
2023-02-22 14:08:53 -0600

    sorting algos attempt

--- 0x1E-search_algorithms/0-linear.c
+++ 0x1E-search_algorithms/0-linear.c
@@ -2,31 +2,29 @@
 #include "search_algos.h"
 
 /**
- * linear_search - sequential search algorithm
+ * linear_search - Searches for a value in an array in integers
+ *
  * @array: a pointer to the first element of the array to search
  * @size: the number of elements in array
  * @value: the value to search for
- * Description: searches for a value in an array of integers
+ *
+ * Return: first index of found value (1), if not (-1)
  */
 
 int linear_search(int *array, size_t size, int value)
 {
-	/* indicates if value is not present in array */
-	if (array == NULL)
-	{
-		return (-1);
-	}
+	size_t i;
 
-	/* compares each element in array to sought value */
-	for (int i = 0; i < size; i++)
+	if (array)
 	{
-		/* if match is found, return index of matching element */
-		printf("Comparing value %d to %d\n", array[i], value);
-		if (array[i] == value)
+		for (i = 0; i < size; i++)
 		{
-			return (i);
+			printf("Value checked array[%lu] = [%i]\n", i, array[i]);
+			if (array[i] == value)
+			{
+				return (i);
+			}
 		}
 	}
-	/* if no match is found, return -1 */
 	return (-1);
 }
--- 0x1E-search_algorithms/1-binary.c
+++ 0x1E-search_algorithms/1-binary.c
@@ -1,7 +1,59 @@
 #include "search_algos.h"
 
 /**
- *  -
- * @:
+ * print_array - Prints an array of integers
  *
+ * @array: The array to be printed
+ * @size: Number of elements in @array
  */
+void print_array(const int *array, size_t size)
+{
+	size_t i;
+
+	i = 0;
+	while (array && i < size)
+	{
+		if (i > 0)
+			printf(", ");
+		printf("%d", array[i]);
+		i++;
+	}
+	printf("\n");
+}
+
+/**
+ * binary_search - Uses binary search to find value in a sorted array of ints
+ *
+ * @array: Pointer to array to seach
+ * @size: Size of array
+ * @value: Value for which to search
+ *
+ * Return: index where value is located
+ */
+int binary_search(int *array, size_t size, int value)
+{
+	size_t i, right = size;
+	size_t left = 0;
+
+	while (array && right > 0)
+	{
+		printf("Searching in array: ");
+		print_array(&array[left], right);
+		i = (right - 1) / 2 + left;
+
+		if (array[i] == value)
+		{
+			return (i);
+		}
+		else if (array[i] < value)
+		{
+			left = i + 1;
+			right /= 2;
+		}
+		else if (array[i] > value)
+		{
+			right = (left - 1) / 2;
+		}
+	}
+	return (-1);
+}
