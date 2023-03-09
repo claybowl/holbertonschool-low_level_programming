21544e26eff251b5d798b8dad8d870b7cc18752a
claybowl <claybowlproductions@gmail.com>
2023-03-08 18:27:12 -0600

    search algo attempt

---
+++ 0x1E-search_algorithms/104-advanced_binary.c
@@ -0,0 +1,60 @@
+#include <stdio.h>
+
+/**
+ * advanced_binary - searches for a value in a sorted array of integers
+ * @array: pointer to the first element of the array to search in
+ * @size: number of elements in array
+ * @value: value to search for
+ * 
+ * Return: index where value is located
+*/
+int advanced_binary(int *array, size_t size, int value)
+{
+	if (array == NULL || size == 0)
+	{
+		return -1;
+	}
+	int middle = size / 2;
+	if (array[middle] == value)
+	{
+		return middle;
+	}
+	else if (array[middle] > value)
+	{
+		// search in left subarray
+		printf("Searching in left subarray: ");
+		for (int i = 0; i < middle; i++)
+		{
+			printf("%d ", array[i]);
+		}
+		printf("\n");
+		int result = advanced_binary(array, middle, value);
+		if (result == -1)
+		{
+			return -1;
+		}
+		else
+		{
+			return result;
+		}
+	}
+	else
+	{
+		// search in right subarray
+		printf("Searching in right subarray: ");
+		for (int i = middle + 1; i < size; i++)
+		{
+			printf("%d ", array[i]);
+		}
+		printf("\n");
+		int result = advanced_binary(array + middle + 1, size - middle - 1, value);
+		if (result == -1)
+		{
+			return -1;
+		}
+		else
+		{
+			return middle + 1 + result;
+		}
+	}
+}
