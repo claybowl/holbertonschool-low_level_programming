27c3583780b5a369f07d4de6bcbd40963f07b30a
claybowl <claybowlproductions@gmail.com>
2023-03-08 19:24:10 -0600

    binary_recursion attempt

--- 0x1E-search_algorithms/1-binary.c
+++ 0x1E-search_algorithms/1-binary.c
@@ -22,38 +22,48 @@ void print_array(const int *array, size_t size)
 }
 
 /**
- * binary_search - Uses binary search to find value in a sorted array of ints
+ * binary_recursion - Uses recursive binary search to find value
+ * in a sorted array of ints
  *
- * @array: Pointer to array to search
- * @size: Size of array
- * @value: Value for which to search
+ * @array: pointer to the first element of the array to search in
+ * @left: left index of section to search
+ * @right: right index of section to search
+ * @value: the value to search for
+ *
+ * Return: index where value is located, or -1
+ */
+int binary_recursion(int *array, int left, int right, int value)
+{
+	size_t mid_point = left + (right - left) / 2;
+
+	if (array && right >= left)
+	{
+		printf("Searching in array: ");
+		print_array(&array[left], right - left + 1);
+		if (array[mid_point] < value)
+		{
+			return (binary_recursion(array, mid_point + 1, right, value));
+		}
+		if (array[mid_point] > value || array[mid_point - 1] == value)
+		{
+			return (binary_recursion(array, left, mid_point, value));
+		}
+		return (mid_point);
+	}
+	return (-1);
+}
+
+/**
+ * advanced_binary - Uses recursive binary search to find value
+ * in a sorted array of ints
+ *
+ * @array: pointer to the first element of the array to search in
+ * @size: the number of elements in array
+ * @value: the value to search for
  *
  * Return: index where value is located
  */
-int binary_search(int *array, size_t size, int value)
+int advanced_binary(int *array, size_t size, int value)
 {
-	size_t i, sub_size = size;
-	size_t sub_start = 0;
-
-	while (array && sub_size > 0)
-	{
-		printf("Searching in array: ");
-		print_array(&array[sub_start], sub_size);
-		i = (sub_size - 1) / 2 + sub_start;
-
-		if (array[i] == value)
-		{
-			return (i);
-		}
-		else if (array[i] < value)
-		{
-			sub_start = i + 1;
-			sub_size /= 2;
-		}
-		else if (array[i] > value)
-		{
-			sub_size = (sub_size - 1) / 2;
-		}
-	}
-	return (-1);
+	return (binary_recursion(array, 0, size - 1, value));
 }
