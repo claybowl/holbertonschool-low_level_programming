2d461a73e1fedbaa3efc427a6e7663b1e8997a25
claybowl <claybowlproductions@gmail.com>
2023-03-08 19:28:22 -0600

    another attempt

--- 0x1E-search_algorithms/104-advanced_binary.c
+++ 0x1E-search_algorithms/104-advanced_binary.c
@@ -1,71 +1,67 @@
-#include <stdio.h>
+#include "search_algos.h"
 
 /**
- * advanced_binary - searches for a value in a sorted array of integers
- * @array: pointer to the first element of the array to search in
- * @size: number of elements in array
- * @value: value to search for
- * 
+ * print_array - Prints an array of integers
+ *
+ * @array: The array to be printed
+ * @size: Number of elements in @array
+ */
+void print_array(const int *array, size_t size)
+{
+	size_t i;
+
+	i = 0;
+	while (array && i < size)
+	{
+		if (i > 0)
+			printf(", ");
+		printf("%d", array[i]);
+		i++;
+	}
+	printf("\n");
+}
+
+/**
+ * binary_recursion - Uses binary search to find value in a sorted array of ints
+ *
+ * @array: Pointer to array to search
+ * @left: Left index of array
+ * @right: Right index of array
+ * @value: Value for which to search
+ *
+ * Return: mid_point where value is located
+ */
+int binary_recursion(int *array, int left, int right, int value)
+{
+	size_t mid_point = left + (right - left) / 2;
+
+	if (array && right >= left)
+	{
+		printf("Searching in array: ");
+		print_array(&array[left], right - left + 1);
+		if (array[mid_point] < value)
+		{
+			return (binary_recursion(array, mid_point + 1, right, value));
+		}
+		if (array[mid_point] > value || array[mid_point - 1] == value)
+		{
+			return (binary_recursion(array, left, mid_point, value));
+		}
+		return (mid_point);
+	}
+	return (-1);
+}
+
+/**
+ * advanced_binary - Uses binary search to find value in a sorted array of ints
+ *
+ * @array: Pointer to array to search
+ * @size: Size of array
+ * @value: Value for which to search
+ *
  * Return: index where value is located
-*/
+ */
 int advanced_binary(int *array, size_t size, int value)
 {
-	int middle;
-	int result;
-	if (array == NULL || size == 0)
-	{
-		return -1;
-	}
-	middle = size / 2;
-	if (array[middle] == value)
-	{
-		return middle;
-	}
-	else if (array[middle] > value)
-	{
-		int i = 0;
-		/* search in left subarray */
-		printf("Searching in left subarray: ");
-		while (i < middle)
-		{
-			printf("%d", array[i]);
-			if (i < middle - 1)
-				printf(", ");
-			i++;
-		}
-		printf("\n");
-		result = advanced_binary(array, middle, value);
-		if (result == -1)
-		{
-			return -1;
-		}
-		else
-		{
-			return result;
-		}
-	}
-	else
-	{
-		size_t i = middle + 1;
-		int result;
-		/* search in right subarray */
-		printf("Searching in right subarray: ");
-		while (i < size)
-		{
-			printf("%d", array[i]);
-			if (i < size - 1)
-				printf(", ");
-			i++;
-		}
-		printf("\n");
-		result = advanced_binary(array + middle + 1, size - middle - 1, value);
-		if (result == -1)
-		{
-			return -1;
-		}
-		else
-		{
-			return middle + 1 + result;
-		}
-	}
+	return (binary_recursion(array, 0, size - 1, value));
 }
